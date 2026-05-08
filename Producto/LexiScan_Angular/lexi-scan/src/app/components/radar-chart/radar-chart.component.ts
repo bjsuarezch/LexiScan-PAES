3ed8761a532bfaccfa64b844c934f7523945e34c
elisyanez <elis.yanez@duocuc.cl>
2026-05-07 20:09:29 -0400

    Mejorado radarchart

--- Producto/LexiScan_Angular/lexi-scan/src/app/components/radar-chart/radar-chart.component.ts
+++ Producto/LexiScan_Angular/lexi-scan/src/app/components/radar-chart/radar-chart.component.ts
@@ -12,25 +12,25 @@ export class RadarChartComponent {
 
   // Definición de colores por región según pediste
   regiones = [
-    { color: 'rgba(144, 238, 144, 0.5)', nombre: 'Verde Claro' }, // Interpretar - Vocabulario
-    { color: 'rgba(255, 165, 0, 0.5)', nombre: 'Naranja' }, // Vocabulario - Tipos de Texto
-    { color: 'rgba(255, 69, 0, 0.5)', nombre: 'Rojo' }, // Tipos de Texto - Localizar
-    { color: 'rgba(147, 112, 219, 0.5)', nombre: 'Púrpura' }, // Localizar - Lectura Crítica
-    { color: 'rgba(64, 224, 208, 0.5)', nombre: 'Turquesa' }, // Lectura Crítica - Evaluar
-    { color: 'rgba(0, 100, 0, 0.5)', nombre: 'Verde Oscuro' }, // Evaluar - Interpretar
+    { color: 'rgba(119, 157, 79, 0.75)', nombre: 'Verde Claro' }, // Interpretar - Vocabulario
+    { color: 'rgba(201, 144, 48, 0.75)', nombre: 'Naranja' }, // Vocabulario - Tipos de Texto
+    { color: 'rgba(168, 54, 55, 0.75)', nombre: 'Rojo' }, // Tipos de Texto - Localizar
+    { color: 'rgba(60, 66, 122, 0.75)', nombre: 'Púrpura' }, // Localizar - Lectura Crítica
+    { color: 'rgba(37, 105, 128, 0.75)', nombre: 'Turquesa' }, // Lectura Crítica - Evaluar
+    { color: 'rgba(63, 116, 101, 0.75)', nombre: 'Verde Oscuro' }, // Evaluar - Interpretar
   ];
 
   getRadarPointsArray(): { x: number; y: number }[] {
     if (!this.habilidades || this.habilidades.length === 0) return [];
 
-    const center = { x: 100, y: 100 };
+    const center = { x: 125, y: 100 };
     const vertices = [
-      { x: 100, y: 40 },
-      { x: 155, y: 70 },
-      { x: 155, y: 130 },
-      { x: 100, y: 160 },
-      { x: 45, y: 130 },
-      { x: 45, y: 70 },
+      { x: 125, y: 40 },
+      { x: 180, y: 70 },
+      { x: 180, y: 130 },
+      { x: 125, y: 160 },
+      { x: 70, y: 130 },
+      { x: 70, y: 70 },
     ];
 
     const order = [
@@ -58,6 +58,6 @@ export class RadarChartComponent {
     if (points.length < 6) return '';
     const p1 = points[index];
     const p2 = points[(index + 1) % 6];
-    return `M 100,100 L ${p1.x},${p1.y} L ${p2.x},${p2.y} Z`;
+    return `M 125,100 L ${p1.x},${p1.y} L ${p2.x},${p2.y} Z`;
   }
 }
